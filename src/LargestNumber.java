0f419dc532dc443ec668affe74dd6a47378998b4
kandavelliVinithKumar <37012080+kandavelliVinithKumar@users.noreply.github.com>
2019-03-02 11:44:09 +0530

    Test2 upload

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -7,26 +7,28 @@ public class LargestNumber {
 		System.out.println("Enter number:");
 		Integer givenNumber=sc.nextInt();
 		System.out.println("Enter digit");
-		int givenDigit=sc.nextInt();
+		Integer givenDigit=sc.nextInt();
 		String sb=givenNumber.toString();
+		String sb2=givenDigit.toString();
 		String temp=sb;
 		Integer k=0;
 		Character c;
-		//for (int i = 0; i < givenNumber; i++) {
+		for (int i = 0; i < givenNumber; i++) {
 			
 			for (int j = 0; j < sb.length(); j++) {
-				System.out.println((temp.charAt(j))+" "+((char) givenDigit));
-				c=(char) givenDigit;
-				if((temp.charAt(j)).compareTo(c)) {
+				//System.out.println((temp.charAt(j))+" "+( givenDigit));
+				
+				if((temp.charAt(j))==sb2.charAt(0)) {
+					//System.out.println("here");
 					k=Integer.parseInt(temp);
 					k--;
 					temp=k.toString();
-					System.out.println(temp);
+				//	System.out.println(temp);
 				}
 				
 			}
 			
-		//}
+		}
 		System.out.println(temp);
 		
 
